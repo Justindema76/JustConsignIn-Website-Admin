78d503c3f2d8b0fca0e52e9334ada8a4a2b02026
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-09-08 22:07:39 -0400

    Load mobile admin overrides

--- src/backend-admin/main.jsx
+++ src/backend-admin/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import AdminApp from './AdminApp';
 import { AdminAuthProvider } from './AdminAuthContext';
 import './backend.css';
+import './mobile.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
