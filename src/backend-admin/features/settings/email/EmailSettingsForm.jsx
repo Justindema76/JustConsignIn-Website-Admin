5dd04b342a4acca72a7a97365ec0d6b8fde66655
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-09-25 17:58:32 -0400

    Use HostPapa certificate hostname for Justin SMTP

--- src/backend-admin/features/settings/email/EmailSettingsForm.jsx
+++ src/backend-admin/features/settings/email/EmailSettingsForm.jsx
@@ -23,7 +23,7 @@ const SITE_PROFILES = {
   },
   justindematteis: {
     label: 'Justin DeMatteis',
-    smtpHost: 'mail.justindematteis.com',
+    smtpHost: 'sh-cp11.yyz2.servername.online',
     smtpUsername: 'justin@justindematteis.com',
     fromEmail: 'justin@justindematteis.com',
     fromName: 'Justin DeMatteis',
