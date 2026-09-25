fa979c651d498f83ec06b9087e728ade45e9dffc
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-09-25 19:11:02 -0400

    Add Hiring Contacts admin route

--- src/backend-admin/app/AdminApp.jsx
+++ src/backend-admin/app/AdminApp.jsx
@@ -7,6 +7,7 @@ import BlogAdmin from '../features/blog/BlogAdmin';
 import Dashboard from '../features/dashboard/Dashboard';
 import DemoRequestsAdmin from '../features/demo-requests/DemoRequestsAdmin';
 import ServiceRequestsAdmin from '../features/service-requests/ServiceRequestsAdmin';
+import HiringContactsAdmin from '../features/hiring-contacts/HiringContactsAdmin';
 import MediaAdmin from '../features/media/MediaAdmin';
 import OutreachMap from '../features/outreach/OutreachMap';
 import SocialImageStudio from '../features/media/SocialImageStudio';
@@ -30,6 +31,7 @@ export default function AdminApp() {
         <Route path="/admin" element={<Dashboard />} />
         <Route path="/admin/demo-requests" element={<DemoRequestsAdmin />} />
         <Route path="/admin/service-requests" element={<ServiceRequestsAdmin />} />
+        <Route path="/admin/hiring-contacts" element={<HiringContactsAdmin />} />
         <Route path="/admin/beta-partners" element={<BetaApplicationsAdmin />} />
         <Route path="/admin/outreach" element={<OutreachMap />} />
         <Route path="/admin/blog" element={<BlogAdmin />} />
